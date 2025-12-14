62867fa0568cbf2dfae315aa40cdd07c35efabeb
whosmitri <hellomitrii@gmail.com>
2025-12-14 11:37:42 -0300

    build: adicionada a pasta com todas as configurações implementadas até então (conexão com banco e formulário)

---
+++ src/config/db/conexao.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "projeto_cursos";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
+?>
---
+++ src/config/salvar_formulario.php
@@ -0,0 +1,17 @@
+<?php
+include 'db/conexao.php';
+
+$area = $_POST['area'];
+$nivel = $_POST['nivel'];
+$objetivo = $_POST['objetivo'];
+
+$sql = "INSERT INTO formularios (area_interesse, nivel, objetivo)
+        VALUES ('$area', '$nivel', '$objetivo')";
+
+if ($conn->query($sql)) {
+    header("Location: recomendacao.php?area=$area&nivel=$nivel");
+    exit;
+} else {
+    echo "Erro ao salvar!";
+}
+?>
