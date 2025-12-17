ba490bb5c753ec6f98d0f1b4380576c4f4216e06
whosmitri <hellomitrii@gmail.com>
2025-12-17 17:37:10 -0300

    feat(index): adicionado o icon da página

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
 
     <link rel="stylesheet" href="./src/style/reset.css">
     <link rel="stylesheet" href="./src/style/global.css">
