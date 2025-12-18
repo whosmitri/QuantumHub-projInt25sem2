de245b884440e5e3b61e1ed9d4db5f94d45032d6
whosmitri <hellomitrii@gmail.com>
2025-12-18 19:06:03 -0300

    feat(cursos): adicionada página de cursos + estilo + responsividade

--- src/pages/cursos.php
+++ src/pages/cursos.php
@@ -19,7 +19,155 @@
 
     <div id="main">
     <main>
-        <section id="callForms"></section>
+        <section id="callForms">
+            <div class="call-forms-container">
+                <h1>Descubra o curso ideal para você!</h1>
+                <p>A tecnologia oferece muitos caminhos — escolher o certo faz toda a diferença.</p>
+                <a class="btn call-action-btn">Descobrir meu curso ideal</a>
+            </div>
+        </section>
+
+        <!-- Container com Todos os Cursos -->
+        <section id="cursosTudo">
+            <div class="cursos-tudo-container">
+                <div class="curso-card">
+                    <h3><a href="./cursos/">Desenvolvimento Web Full Stack</a></h3>
+                    <p>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-layers-1"></i>
+                            12 Módulos
+                        </span>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-alarm-1"></i>
+                            80h
+                        </span>
+                    </p>
+                </div>
+
+                <div class="curso-card">
+                    <h3><a href="./cursos/">Ciência de Dados com Python</a></h3>
+                    <p>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-layers-1"></i>
+                            10 Módulo
+                        </span>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-alarm-1"></i>
+                            60h
+                        </span>
+                    </p>
+                </div>
+
+                <div class="curso-card">
+                    <h3><a href="./cursos/">Cibersegurança para Iniciantes</a></h3>
+                    <p>
+                        <span class="curso-total-modulos">
+                                <i class="lni lni-layers-1"></i>
+                                8 Módulos
+                        </span>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-alarm-1"></i>
+                            40h
+                        </span>
+                    </p>
+                </div>
+
+                <div class="curso-card">
+                    <h3><a href="./cursos/">Automação com Python</a></h3>
+                    <p>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-layers-1"></i>
+                            7 Módulos
+                        </span>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-alarm-1"></i>
+                            35h
+                        </span>
+                    </p>
+                </div>
+
+                <div class="curso-card">
+                    <h3><a href="./cursos/">Curso de Cloud Computing</a></h3>
+                    <p>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-layers-1"></i>
+                            9 Módulos
+                        </span>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-alarm-1"></i>
+                            45h
+                        </span>
+                    </p>
+                </div>
+
+                <div class="curso-card">
+                    <h3><a href="./cursos/">Banco de Dados SQL e NoSQL</a></h3>
+                    <p>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-layers-1"></i>
+                            8 Módulos
+                        </span>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-alarm-1"></i>
+                            40h
+                        </span>
+                    </p>
+                </div>
+
+                <div class="curso-card">
+                    <h3><a href="./cursos/">Desenvolvimento Mobile</a></h3>
+                    <p>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-layers-1"></i>
+                            10 Módulos
+                        </span>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-alarm-1"></i>
+                            60h
+                        </span></p>
+                </div>
+
+                <div class="curso-card">
+                    <h3><a href="./cursos/">Marketing Digital e SEO</a></h3>
+                    <p>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-layers-1"></i>
+                            7 Módulos
+                        </span>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-alarm-1"></i>
+                            30h
+                        </span></p>
+                </div>
+                
+                <div class="curso-card">
+                    <h3><a href="./cursos/">E-commerce e Vendas Online</a></h3>
+                    <p>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-layers-1"></i>
+                            6 Módulos
+                        </span>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-alarm-1"></i>
+                            25h
+                        </span></p>
+                </div>
+                
+                <div class="curso-card">
+                    <h3><a href="./cursos/">Inteligência Artificial Generativa</a></h3>
+                    <p>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-layers-1"></i>
+                            6 Módulos
+                        </span>
+                        <span class="curso-total-modulos">
+                            <i class="lni lni-alarm-1"></i>
+                            24h
+                        </span></p>
+                </div>
+            </div>
+        </section>
+        <!-- Fim do Container com Todos os Cursos -->
     </main>
     </div>
 
