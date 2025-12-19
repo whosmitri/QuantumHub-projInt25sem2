59c4ecbdc40f4dba1cc25bbfd7f5bd72879b2781
whosmitri <hellomitrii@gmail.com>
2025-12-19 17:19:48 -0300

    style(recomendacao): adicionado o estilo da página de recomendação do curso

--- src/pages/recomendacao.php
+++ src/pages/recomendacao.php
@@ -8,14 +8,39 @@ $cursos = [
     "Cloud" => "Curso de Cloud Computing"
 ];
 
-$area = $_GET['area'];
+//$area = $_GET['area'];
+$area = 'Cibersegurança';
 
 $recomendado = $cursos[$area];
 ?>
 
-<h1>Curso recomendado para você</h1>
-<h2><?= $recomendado ?></h2>
 
-<a href="comprar.php?curso=<?= urlencode($recomendado) ?>">Comprar agora</a>
-<br>
-<a href="cursos.php">Ver todos os cursos</a>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php include './components/head.php'; ?>
+
+    <link rel="stylesheet" href="../styles/recomendacao.css">
+
+    <title>Recomendação - QuantumHub</title>
+</head>
+
+<body>
+    <?php include './components/header.php'; ?>
+
+    <div id="main">
+    <main>
+
+        <h1>Curso recomendado para você</h1>
+        <h2><?= $recomendado ?></h2>
+
+        <a href="comprar.php?curso=<?= urlencode($recomendado) ?>" class="btn main-btn">Comprar agora</a>
+        <a href="cursos.php">Ver todos os cursos</a>
+
+    </main>
+    </div>
+
+    <?php include './components/footer.php'; ?>
+</body>
+
+</html>
